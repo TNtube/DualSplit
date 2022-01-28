623aeef04d4748578ee3eb6185bbb787978ed46f
TNtube <51389578+TNtube@users.noreply.github.com>
2022-01-28 23:36:59 +0100

    Add basic example test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	
+	private float _axisX;
+	private float _axisY;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update() {
+		_axisX = Input.GetAxis("Horizontal");
+		_axisY = Input.GetAxis("Vertical");
+	}
+
+	private void FixedUpdate() {
+		transform.Translate(_axisX * speed * Time.deltaTime, _axisY * speed * Time.deltaTime, 0);
+	}
+}
