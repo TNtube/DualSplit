9b64cf56c06d50ecbafc4a9ee400b02655ae231d
TNtube <51389578+TNtube@users.noreply.github.com>
2022-01-30 13:57:58 +0100

    Implement assets to arrow shooter

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -7,9 +7,39 @@ public class Arrow : MonoBehaviour {
 	public float lifetime;
 	
 	public float arrowSpeed;
+	public Sprite[] arrowSprites;
+	
 	internal Vector2 Velocity;
 
 	private int _canMove = 1;
+
+	private Sprite arrowSprite;
+
+	private void Start() {
+		switch (Velocity.x) {
+			case 1 :
+				arrowSprite = arrowSprites[0];
+				break;
+			case -1:
+				arrowSprite = arrowSprites[1];
+				break;
+			default:
+				switch (Velocity.y) {
+					case 1 :
+						arrowSprite = arrowSprites[2];
+						break;
+					case -1:
+						arrowSprite = arrowSprites[3];
+						break;
+					default:
+						print("not a good dir");
+						break;
+				}
+				break;
+		}
+
+		GetComponent<SpriteRenderer>().sprite = arrowSprite;
+	}
 
 	private void Update() {
 		if (lifetime <= 0) Destroy(gameObject);
@@ -21,8 +51,11 @@ public class Arrow : MonoBehaviour {
 	}
 
 	private void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag("ArrowShooter") || col.CompareTag("Arrow")) return;
 		if (!col.CompareTag("Player")) {
 			_canMove = 0;
+			
+			print(col.gameObject.name);
 			
 			if (!col.CompareTag("Arrow"))
 				transform.SetParent(col.transform);
