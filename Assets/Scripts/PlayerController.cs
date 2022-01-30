1b396ab22abb8f9419d89ea31a1a87a85ea000ef
RavenSKin <59887798+RavenSKin@users.noreply.github.com>
2022-01-30 15:43:11 +0100

    Les animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,14 +13,30 @@ public class PlayerController : MonoBehaviour {
 
 	public bool mirroredPlayer;
 
+	private Animator character_Animator;
 	[SerializeField]
 	private bool _isMirror;
 
-	// Update is called once per frame
+    private void Start()
+    {
+		character_Animator = gameObject.GetComponent<Animator>();  
+    }
+    // Update is called once per frame
     void Update() {
 		_axisX = Input.GetAxis("Horizontal");
 		_axisY = Input.GetAxis("Vertical");
+		if(_axisX!=0 || _axisY != 0)
+        {
+			character_Animator.SetBool("IsWalking", true);
+			character_Animator.SetFloat("X", _axisX);
+			character_Animator.SetFloat("Y", _axisY);
 
+        }
+        if(_axisX == 0 && _axisY == 0)
+        {
+			character_Animator.SetBool("IsWalking", false);
+
+		}
 		if (Input.GetButtonDown("Jump") && SceneManager.GetActiveScene().buildIndex >= 3) {
 			_isMirror = !_isMirror;
 		}
